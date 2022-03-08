49c69dcbf936943485ff5227ba6fc079880e7d51
TTRademacher <rademacher.tim@gmail.com>
2022-03-08 10:02:11 -0500

    Add a home page to the app.

---
+++ acer-web/acceuil.md
@@ -0,0 +1,15 @@
+**Acer-web** est un projet de science ouverte et de science citoyenne pour mieux comprendre la coulée de sève des érables. La plateforme de **Acer-web** a été construit par Tim Rademacher. 
+
+### Qu'est-ce que Acer-web ?
+
+### Pourquoi participer ?
+
+### Qui peut participer ?
+
+Tout le monde peut participer
+
+### Comment puis-je participer ?
+
+### Qu'est-ce que j'ai besoin pour participer ?
+
+### Que faites-vous avec les données ?
--- acer-web/ui.R
+++ acer-web/ui.R
@@ -13,75 +13,106 @@ shinyUI(fluidPage(
     # Application title
     titlePanel("Acer web"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            selectInput(
-                inputId = "site",
-                label = "Site",
-                choices = c("1 - L'Assomption","2 - Bolton","3 - Beauce","4 - Montréal (Mercier)","5 - Montréal Sud-Ouest"),
-                selected = NULL,
-                multiple = FALSE,
-                selectize = TRUE,
-                width = NULL,
-                size = NULL
-            ),
-            dateInput(
-                inputId = "date",
-                label = "Date de mesure",
-                value = NULL,
-                min = "2022-01-01",
-                max = "2022-12-31",
-                format = "yyyy-mm-dd",
-                startview = "month",
-                weekstart = 1, # monday is first day of week
-                language = "fr",
-                width = NULL,
-                autoclose = TRUE,
-                datesdisabled = NULL,
-                daysofweekdisabled = NULL
-            ),
-            shinyTime::timeInput(inputId = "start_time", 
-                      label = "Temps de départ", 
-                      value = NULL, 
-                      seconds = FALSE,
-                      minute.steps = NULL),
-            shinyTime::timeInput(inputId = "end_time", 
-                      label = "Temps de fin", 
-                      value = NULL, 
-                      seconds = FALSE,
-                      minute.steps = NULL),
-            numericInput(inputId = "n_trees",
-                         label = "Nombre d'arbre:",
-                         min = 1,
-                         max = 100,
-                         value = 10),
-            numericInput(inputId = "t_air_C",
-                         label = "Température d'aire approximative (°C):",
-                         min = -40,
-                         max = 30,
-                         value = 0),
-            checkboxInput(inputId = "precip",
-                          label = "Pleuvait-il pendant les mesures ?",
-                          value = FALSE, width = NULL),
-            numericInput(inputId = "cloudiness",
-                         label = "Couverture de nuage (%):",
-                         min = 0,
-                         max = 100,
-                         value = 50, 
-                         width = "45%"),
-            numericInput(inputId = "snow_cover",
-                         label = "Couverture de neige (%):",
-                         min = 0,
-                         max = 100,
-                         value = 50, 
-                         width = "45%")
+    tabsetPanel(
+        br (), # insert some space
+        
+        # Home tab
+        tabPanel("Acceuil", fluid = TRUE,
+                 includeMarkdown("acceuil.md") 
         ),
+        # Data entry tab
+        tabPanel("Saisie de données", fluid = TRUE,
+            sidebarLayout(
+                sidebarPanel(
+                    selectInput(
+                        inputId = "site",
+                        label = "Site",
+                        choices = c("1 - L'Assomption","2 - Bolton","3 - Beauce","4 - Montréal (Mercier)","5 - Montréal Sud-Ouest"),
+                        selected = NULL,
+                        multiple = FALSE,
+                        selectize = TRUE,
+                        width = NULL,
+                        size = NULL
+                    ),
+                    
+                    hr (), # horizontal line
+                    dateInput(
+                        inputId = "date",
+                        label = "Date de mesure",
+                        value = NULL,
+                        min = "2022-01-01",
+                        max = "2022-12-31",
+                        format = "yyyy-mm-dd",
+                        startview = "month",
+                        weekstart = 1, # monday is first day of week
+                        language = "fr",
+                        width = NULL,
+                        autoclose = TRUE,
+                        datesdisabled = NULL,
+                        daysofweekdisabled = NULL
+                    ),
+                    strong ("Temps de mesures approximatif"),
+                    splitLayout(cellWidths = c (160,160),
+                        shinyTime::timeInput(inputId = "start_time", 
+                                  label = "", 
+                                  value = NULL, 
+                                  seconds = FALSE,
+                                  minute.steps = NULL),
+                        shinyTime::timeInput(inputId = "end_time", 
+                                  label = "", 
+                                  value = NULL, 
+                                  seconds = FALSE,
+                                  minute.steps = NULL)
+                    ),
+                    radioButtons(inputId = "flow",
+                                 label = "Est-ce que la sève a coulée depuis la dernière mesure ?",
+                                 choices = c ("Non", "Oui"),
+                                 selected = "Non", 
+                                 inline = TRUE),
+                    
+                    hr (), # horizontal line
+                    strong ("Météo pendant les mesures"),
+                    br (), 
+                    
+                    numericInput(inputId = "t_air_C",
+                                 label = "Température d'aire (°C):",
+                                 min = -40,
+                                 max = 30,
+                                 value = 0,
+                                 width = "50%"),
+                    radioButtons(inputId = "precip",
+                                 label = "Avait-il de la précipitation pendant les mesures ?",
+                                 choices = c ("Non", "Pluie", "Neige"),
+                                 selected = "Non", 
+                                 inline = TRUE),
+                    splitLayout(
+                        numericInput(inputId = "cloudiness",
+                                     label = "Couverture de nuage (%):",
+                                     min = 0,
+                                     max = 100,
+                                     value = 50, 
+                                     width = "45%"),
+                        numericInput(inputId = "snow_cover",
+                                     label = "Couverture de neige (%):",
+                                     min = 0,
+                                     max = 100,
+                                     value = 50, 
+                                     width = "45%")),
+                    
+                    # horizontal line
+                    hr (),
+                    
+                    # submit button
+                    actionButton (inputId = "confirmSiteData",
+                                  label = "Confirmer les observations",
+                                  inline = TRUE)
+                    ),
 
-        # Show a plot of the past data from the site with a line for each tree
-        mainPanel(
-            plotOutput("distPlot")
-        )
+                # Show a plot of the past data from the site with a line for each tree
+                mainPanel(
+                    plotOutput("distPlot")
+                )
+            ))
     )
 ))
 #===============================================================================
