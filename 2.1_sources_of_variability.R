be972cbe4ebd0f5b472c7edb857694b29185c34a
TTRademacher <rademacher.tim@gmail.com>
2022-08-30 11:51:27 -0400

    Started integrating the quantification of sources of variability.

---
+++ 2.1_sources_of_variability.R
@@ -0,0 +1,15 @@
+#===============================================================================
+#
+# Section 2.1: Sources of variability
+#
+# script to quantify the relative importance of various factors in introducing 
+# variability in sap yield and sugar content. Factors we account for are:
+# - year
+# - site
+# - tree
+# - tap
+# - species
+#-------------------------------------------------------------------------------
+
+# load the wrangled data -------------------------------------------------------
+source("0_wrangle_data.R")
